0606a8f5781bc9444eb196c453c96c7140106d7e
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-15 05:05:41 +0600

    full db and baackend setup done

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express, { Application, Request, Response } from "express";
+import cors from 'cors'
+
+const app: Application = express();
+
+app.use(cors())
+app.use(express.json());
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Server is running!");
+});
+
+export default app;
