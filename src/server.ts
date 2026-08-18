2d675a0a04b9edc4f690c1975f87d553a565e1ab
tfshorifulislam <tfshorifulislam@gmail.com>
2026-08-18 16:55:23 +0600

    fix production prisma schema

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,23 @@
 import "dotenv/config";
 import app from "./app.js";
+import { prisma } from "./lib/prisma.js";
 
 const PORT = process.env.PORT || 8000;
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+async function startServer() {
+    try {
+        await prisma.$queryRaw`SELECT 1`;
+
+        console.log("✅ DATABASE CONNECTED");
+
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`);
+        });
+    } catch (error) {
+        console.error("❌ DATABASE CONNECTION FAILED");
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+startServer();
